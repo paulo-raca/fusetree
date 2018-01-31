150655523f536f7633286b5c17a96979f9aea305
Paulo Costa <me@paulo.costa.nom.br>
2018-01-31 21:40:26 -0200

    Restructure code as a package

---
+++ fusetree/__init__.py
@@ -0,0 +1,6 @@
+from fusetree.core import Stat, StatVFS, Path, generatorfile
+from fusetree.core import Bytes_Like, Stat_Like, StatVFS_Like, Node_Like, FileHandle_Like, DirEntry, DirHandle_Like
+from fusetree.core import Path, Node, RootNode, DirHandle, FileHandle
+
+from fusetree.core import BlobFile, GeneratorFile, generatorfile, UrllibFile, DictDir
+from fusetree.core import FuseTree

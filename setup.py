07bad8d306ad756fb0c360bd32d43e84ae553609
Paulo Costa <me@paulo.costa.nom.br>
2018-02-01 18:48:55 -0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'fusetree',
+    packages = ['fusetree'],
+    version = '0.1.0',
+    description = 'Hig-level API for fusepy',
+    author = 'Paulo Costa',
+    author_email = 'me@paulo.costa.nom.br',
+    url = 'https://github.com/paulo-raca/fusetree',
+    download_url = 'https://github.com/paulo-raca/fusetree',
+    keywords = ['fuse'],
+    install_requires = [
+        "fusepy"
+    ]
+)

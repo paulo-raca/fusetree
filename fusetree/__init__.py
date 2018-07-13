cb42d124734772e0927cd4e8aa8434008cca71a3
Paulo Costa <me@paulo.costa.nom.br>
2018-07-13 19:10:56 -0300

    class RootNode no longer exists

--- fusetree/__init__.py
+++ fusetree/__init__.py
@@ -1,6 +1,6 @@
 from fusetree.types import Stat, StatVFS, Path
 from fusetree.types import Bytes_Like, Stat_Like, StatVFS_Like, Node_Like, FileHandle_Like, DirEntry, DirHandle_Like
-from fusetree.core import Node, RootNode, DirHandle, FileHandle
+from fusetree.core import Node, DirHandle, FileHandle
 from fusetree.fusetree import FuseTree
 
 from fusetree.nodetypes import BaseSymlink, BaseFile, BaseDir, Symlink, BlobFile, GeneratorFile, generatorfile, HttpFile, DictDir
